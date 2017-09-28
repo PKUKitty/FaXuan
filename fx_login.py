9881e2194a25ba2bcced4f9a790ba92794895f06
yujun <pkukitty@gmail.com>
2017-09-28 20:21:09 +0800

    add login codes

--- fx_login.py
+++ fx_login.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 import urllib
 import urllib2
 
@@ -6,6 +7,7 @@ import cookielib
 import time
 import os.path
 
+from pytesseract import pytesseract
 from selenium import webdriver
 
 try:
@@ -14,8 +16,8 @@ except:
     pass
 
 accept = 'application/json, text/javascript, */*; q=0.01'
-# agent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'
-agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36';
+agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/61.0.3163.79 Chrome/61.0.3163.79 Safari/537.36'
+# agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36';
 
 headers = {
     'Accept': '*/*',
@@ -112,6 +114,10 @@ def get_cookies():
     return res.cookies
 
 
+def ger_random_qq():
+    return '12345678'  # TODO
+
+
 if __name__ == '__main__':
     user_name = '15094279360'
     password = 'y888888'
@@ -119,17 +125,20 @@ if __name__ == '__main__':
 
     # login(user_name, password)
 
-    driver = webdriver.Chrome()
-    driver.get('http://www.faxuan.net/site/yunnan/')
+    driver = webdriver.Firefox()
+    driver.get(HOME_PAGE_URL)
+    time.sleep(1)
+    cookie1 = driver.get_cookies()
 
     elem_user = driver.find_element_by_id('user_name')
     elem_psw = driver.find_element_by_id('user_pass')
     elem_code = driver.find_element_by_id('code')
 
-    image_name = '/Users/yujun/PycharmProjects/FaXuan/login.png'
+    # image_name = '/Users/yujun/PycharmProjects/FaXuan/login.png'
+    image_name = 'home_page.png'
     driver.get_screenshot_as_file(image_name)
     img = Image.open(image_name)
-    box = (555, 750, 680, 830)
+    box = (285, 380, 345, 415)
     region = img.crop(box)
     region.save('captcha.png')
     im = Image.open('captcha.png')
@@ -138,12 +147,133 @@ if __name__ == '__main__':
     sharp_img = sharpness.enhance(2.0)
     sharp_img.save('captcha.png')
 
-    driver.quit()
+    # captcha = raw_input("pls input the captcha\n>")
+    captcha = pytesseract.image_to_string(sharp_img)
+    print 'captcha:'
+    print captcha
 
-    captcha = raw_input("pls input the captcha\n>")
-    elem_user.send_keys(user_name)
-    elem_psw.send_keys(password)
-    elem_code.send_keys(captcha)
-    click_login = driver.find_element_by_class_name('login_button')
-    click_login.click()
+    is_login = False
+    if captcha:
+        elem_user.send_keys(user_name)
+        elem_psw.send_keys(password)
+        elem_code.send_keys(captcha)
+        click_login = driver.find_element_by_class_name('login_button')
+        click_login.click()
+        cookie2 = driver.get_cookies()
+        if cookie1 != cookie2:
+            is_login = True
+
+    else:
+        driver.quit()
+        exit()
+    # if not is_login:
+    #     print 'login failed'
+    #     driver.quit()  # TODO
+    #     exit()
+
+    # click to modify profile,
+    time.sleep(2)
+    current_window = driver.current_window_handle
+    modifyProfile = driver.find_element_by_xpath("//a[contains(@href, ‘navindex’)]")
+    modifyProfile.get_attribute("href")
+    modifyProfile.click()
+    user_info_qq = driver.find_element_by_id('userinfoQQ')
+    user_info_qq.send_keys(ger_random_qq())
+
+    user_info_save = driver.find_element_by_id('userInfo_2_queren')
+    user_info_save.click()
+    # time sleep
+    time.sleep(1)
+
+    user_info_confirm = driver.find_element_by_id('popalertConfirm')
+    user_info_confirm.click()
+    time.sleep(3)
+
+    time.sleep(10)
+    driver.quit()
+    exit()
+
+    my_study = driver.find_element_by_link_text('javascript:base.index2(1)')
+    my_study.click()
+    time.sleep(3)
+
+    # click must course
+    must_courses = driver.find_element_by_id('bxCourse')
+    must_courses.click()
+
+    # start study
+    # include all must courses
+    country_security = driver.find_element_by_link_text('中华人民共和国国家安全法》...')
+    country_security.click()
+    time.sleep(3)
+
+    start = time.time()
+    duration = 30 * 60
+    # country_security_timer = driver.find_element_by_id('timer')
+    while True:
+        if time.time() - start > duration:
+            break
+        else:
+            time.sleep(10)
+            continue
+    exit_study = driver.find_element_by_link_text('退出学习')
+    exit_study.click()
+
+    exit_study_confirm = driver.find_element_by_id('popwinConfirm')
+    exit_study_confirm.click()
+    time.sleep(1)
+
+    # must courses
+    constitution = driver.find_element_by_link_text('宪法知识读本')
+    constitution.click()
+    time.sleep(3)
+
+    start = time.time()
+    while True:
+        if time.time() - start > duration:
+            break
+        else:
+            time.sleep(10)
+            continue
+    constitution_exit_study = driver.find_element_by_link_text('退出学习')
+    constitution_exit_study.click()
+
+    constitution_exit_study_confirm = driver.find_element_by_id('popwinConfirm')
+    constitution_exit_study_confirm.click()
+    time.sleep(1)
+
+    # must courses
+    yunnan = driver.find_element_by_link_text('云南省农村扶贫开发条例')
+    yunnan.click()
+    time.sleep(3)
+
+    start = time.time()
+    while True:
+        if time.time() - start > duration:
+            break
+        else:
+            time.sleep(10)
+            continue
+    yunnan_exit_study = driver.find_element_by_link_text('退出学习')
+    yunnan_exit_study.click()
+
+    yunnan_exit_study_confirm = driver.find_element_by_id('popwinConfirm')
+    yunnan_exit_study_confirm.click()
+    time.sleep(1)
+
+    # start
+    country_security = driver.find_element_by_link_text('中华人民共和国国家安全法》...')
+    country_security.click()
+    time.sleep(3)
+
+    course_exs = driver.find_element_by_link_text('课程练习')
+    course_exs.click()
+
+    start_exs = driver.find_element_by_link_text('开始练习')
+    start_exs.click()
+
+    time.sleep(20)
+    submit_exs = driver.find_element_by_link_text('提交练习')
+    submit_exs.click()
+
     driver.quit()
---
+++ timer.py
@@ -0,0 +1,8 @@
+import time
+
+class Timer:
+    start = 0
+    end = 0
+
+    def start(self):
+        start = time.time()
