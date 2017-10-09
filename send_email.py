a1c376b72d364325592e1b6d8c6ba71fdea3d378
yujun <pkukitty@gmail.com>
2017-10-09 11:18:48 +0800

    recode important info, including password

--- send_email.py
+++ send_email.py
@@ -1,19 +1,30 @@
+import json
 import smtplib
 from email.header import Header
 from email.mime.text import MIMEText
+
+
+def parse_config(file_name):
+    config_file = open(file_name)
+    json_config = json.load(config_file)
+    config_file.close()
+    return json_config
 
 
 class SendEmail:
     def __init__(self):
         pass
 
-    user_name = 'yujun@travelsky.com'
-    user_password = 'Yj1314520'
+    CONFIG_FILE = "/home/yujun/PycharmProjects/FaXuan/fx_login.conf"
+    config = parse_config(CONFIG_FILE)
+    email_info = config['email']
+    user_name = email_info['user_name']
+    user_password = email_info['password']
 
     subject = 'fx login'
-    sender = 'yujun@travelsky.com'
-    receiver = 'yujun@travelsky.com'
-    smtp_server = 'smtpav.travelsky.com'
+    sender = email_info['sender']
+    receiver = email_info['receiver']
+    smtp_server = email_info['smtp_server']
 
     def send_msg(self, msg_content):
         msg = MIMEText(msg_content, 'text', 'UTF-8')
